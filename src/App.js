47111ae95066763cfe3cc56a891daa7b2bad1915
Bast <bast@Gilkas-iMac.local>
2018-09-26 21:25:53 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Web-Wesh</h1>
           <p style={{"color":"#777"}}>Créons ensemble le prochain Wesh !</p>
+          <p> OUAI !</p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
